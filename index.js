fd8a771d497da7b99cb96b6a60f5b90275613d7f
Zahurul Islam <zahurulislam803@gmail.com>
2023-11-08 01:17:14 +0600

    get all rooms data

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ async function run() {
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false,
+          secure: true,
           sameSite: 'none'
         })
         .send({ success: true })
